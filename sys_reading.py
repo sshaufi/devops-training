570c23cc36bf077bf0cccd60b4f59d42c8b42261
Shaufi Shamsul <shaufishamsul@gmail.com>
2024-03-29 21:51:21 +1000

    Initial commit

---
+++ sys_reading.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import os
+import platform
+import psutil
+
+# Variable
+if platform.system() == "Darwin":
+    private_ip = (os.popen("ifconfig|awk ' /inet / && /broadcast/ {print $2 } '").read().strip())
+else:
+    private_ip = "okey"
+
+disk = psutil.disk_usage('/')
+root_total = format(disk.total/1024**3,".1f")
+root_used = format(disk.used/1024**3,".1f")
+
+# General
+print("Hostname: ", os.popen("hostname").read().strip())
+# Using uptime instead, psutil boot_time() is hard parse, will use fully awk instead of cut
+print("Uptime:", os.popen("uptime| awk -F ',' '{print $1}'|cut -f5- -d ' '").read().strip())
+print("CPU (%):", psutil.cpu_percent(0.5))
+print("Mem Used (GB):", format(psutil.virtual_memory()[3]/1024**3, ".1f"),"/",format(psutil.virtual_memory()[0]/1024**3,".1f"))
+
+# Network
+print("Private IP:", private_ip)
+
+# Will add conditioin where condition where it'll use wget, not all system have curl
+print("Public IP:", os.popen("curl -s ifconfig.me").read().strip())
+
+# Disk
+print("Disk Usage '/' (GB):",root_used,"/",root_total)
+
+#TODO
+# Security, firewall
+
